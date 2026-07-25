385e83a17a6defb0caecebba2947528d6ee6aed4
Phuc Truong <truonghoangphuc27012005@gmail.com>
2026-07-25 13:43:47 +0700

    feat(nguoi dung): module quan ly tai khoan nhan vien
    - Danh sach card-row: avatar chu cai, ho ten, email, badge vai tro + trang thai
    - Tim theo ten-email, chip loc theo vai tro
    - Dialog tao: ho ten, email, vai tro, mat khau (PasswordBox doc o code-behind,
      khong luu mat khau vao property/log) + huong dan mat khau manh
    - Sua thong tin, khoa/mo tai khoan, dat lai mat khau
    - Khong cho tu khoa tai khoan dang dang nhap (an nut + service chan)
    - Chi Admin truy cap

---
+++ FUHotelManagementWPF/Views/Users/UserListView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUHotelManagementWPF.Views.Users;
+
+public partial class UserListView : UserControl
+{
+    public UserListView()
+    {
+        InitializeComponent();
+    }
+}
